4a8709d8f6a78b600c310573df99cd2019e19b4d
Adrian Juel Boncodin <juelboncodin@gmail.com>
2025-05-21 03:23:56 +0800

    Jsonwebtoken apply

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI , { dbName: 'Ecommerce' })
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) { 
         console.log("Error connecting to MongoDB" , error.message)
+        process.exit(1)
     }
 }
